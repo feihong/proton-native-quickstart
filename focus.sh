dbff682857346dfaa138024d31c12269e7021af8
Feihong Hsu <feihong.hsu@gmail.com>
2018-12-26 12:03:55 -0600

    Make it possible to quit using keyboard shortcut

--- focus.sh
+++ focus.sh
@@ -1,4 +1,4 @@
-sleep 0.5
+sleep 1
 osascript -e 'tell application "System Events" to tell process "node"' \
   -e 'set frontmost to true' \
   -e 'end tell'
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
-
-import { render, Window, App, Box, Button, Separator, TextInput } from 'proton-native';
+import React, { Component } from 'react'
+import { render, Window, App, Menu, Box, Button, Separator, TextInput }
+  from 'proton-native'
 
 class Demo extends Component {
   render() {
     return (
-      <App>
-        <Window title="Demo" size={{ w: 300, h: 300 }}>
+      <App onShouldQuit={() => console.log('quitting')}>
+        <Menu>
+          <Menu.Item type='Quit' />
+        </Menu>
+        <Window title="Demo" size={{ w: 300, h: 300 }} menuBar={false}>
           <Box>
             <Button stretchy={true} onClick={() => console.log('Hello')}>
               Button
